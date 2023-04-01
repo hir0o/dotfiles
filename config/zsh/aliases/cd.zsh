ebf5dfef1a2aea345e81366fa89104a6214e25d1
hir0o <mk.9.q.kyu@gmail.com>
2023-04-01 13:16:33 +0900

    ディレクトリ移動のコマンドの整理
    gpt-4さんありがとう。

---
+++ config/zsh/aliases/cd.zsh
@@ -0,0 +1,48 @@
+bat_options="bat --color=always --style=header,grid --line-range :80"
+
+fzf_select() {
+  local root=$1
+  local preview_cmd=$2
+  local dir_list_cmd=$3
+
+  local dir=$(
+    eval $dir_list_cmd | \
+    fzf --preview "$preview_cmd"
+  )
+
+  if [[ -n $root ]]; then
+    echo $root/$dir
+  else
+    echo $dir
+  fi
+}
+
+cdf() {
+  local dir=$(fzf_select "" "$bat_options {}/README.*" "ls -d -- */")
+  if [[ -n $dir ]]; then
+    cd $dir
+  fi
+}
+
+cg() {
+  local root=$(ghq root)
+  local dir=$(fzf_select $root "$bat_options $root/{}/README.*" "ghq list")
+  if [[ -n $dir && $dir != $root/ ]]; then
+    cd $dir
+  fi
+}
+
+ccg() {
+  local root=$(ghq root)
+  local dir=$(fzf_select $root "$bat_options $root/{}/README.*" "ghq list")
+  if [[ -n $dir && $dir != $root/ ]]; then
+    code $dir
+  fi
+}
+
+cgp() {
+  local dir=$(fzf_select "" "$bat_options {}/README.*" "ls -d ~/projects/*/* | sed '/\./d'")
+  if [[ -n $dir ]]; then
+    cd $dir
+  fi
+}
--- config/zsh/aliases/git.zsh
+++ config/zsh/aliases/git.zsh
@@ -1,4 +1,1 @@
-alias g='git'
-alias cg='cd $(ghq root)/$(ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")'
-alias cgp='cd `ls -d ~/projects/*/* | sed "/\./d" | fzf --preview "bat --color=always --style=header,grid --line-range :80 {}/README.*"`'
-alias ccg='code $(ghq root)/$(ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 $(ghq root)/{}/README.*")'
+alias g=git

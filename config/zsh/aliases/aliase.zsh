5fbcfad29d91e79f83a7e595a9ce08a86de0e321
Hiroyuki Shibuya <mk.9.q.kyu@gmail.com>
2026-01-02 20:48:44 +0900

    feat: migrate to GNU Stow and optimize configs

--- config/zsh/aliases/aliase.zsh
+++
@@ -1,6 +0,0 @@
-alias vial='nvim `ls -d ${DOTPATH}/aliases/* | fzf --preview "bat --color=always --style=header,grid {}"`'
-alias sral='source `ls -d ${DOTPATH}/aliases/* | fzf --preview "bat --color=always --style=header,grid {}"`'
-alias fmv='fzf --multi | xargs -I{} mv {}'
-
-alias c="cursor"
-alias v="code"
---
+++ packages/zsh/.config/zsh/aliases/aliase.zsh
@@ -0,0 +1,7 @@
+alias vial='nvim `ls -d ${DOTPATH}/aliases/* | fzf --preview "bat --color=always --style=header,grid {}"`'
+alias sral='source `ls -d ${DOTPATH}/aliases/* | fzf --preview "bat --color=always --style=header,grid {}"`'
+alias fmv='fzf --multi | xargs -I{} mv {}'
+
+alias c="cursor"
+alias v="code"
+alias zj="zellij"

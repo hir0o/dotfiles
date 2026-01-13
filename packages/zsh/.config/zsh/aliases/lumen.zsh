439afc6ac303686e6e1da30defce92ce916e246d
Hiroyuki Shibuya <mk.9.q.kyu@gmail.com>
2026-01-13 23:14:56 +0900

    feat(zsh): Lumen用エイリアスを追加

---
+++ packages/zsh/.config/zsh/aliases/lumen.zsh
@@ -0,0 +1,6 @@
+# Lumen - AI-powered git commit summaries
+alias lm='lumen'
+alias lme='lumen explain'
+alias lml='lumen list'
+alias lmd='lumen diff'
+alias lmdr='lumen draft -c "日本語でコミットメッセージを生成してください"'
